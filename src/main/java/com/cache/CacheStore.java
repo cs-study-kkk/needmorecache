4327551875845630b029f6f9cfcde495aa42736d
한권희 <jana32496@gmail.com>
2025-11-16 14:52:34 +0900

    feat(cache): CacheEntry, CacheStore 기본 로직 구현 #3

--- src/main/java/com/cache/CacheStore.java
+++ src/main/java/com/cache/CacheStore.java
@@ -1,4 +1,52 @@
 package com.cache;
 
-public class CacheStore {
+import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
+
+public class CacheStore<K, V> {
+    private final Map<K, CacheEntry<V>> store = new ConcurrentHashMap<>();
+
+    // no TTL
+    public void set(K key, V value) {
+        set(key, value, -1);
+    }
+
+    public void set(K key, V value, long ttl) {
+        store.put(key, new CacheEntry<>(value, ttl));
+    }
+
+    public V get(K key) {
+        CacheEntry<V> entry = store.get(key);
+
+        if (entry == null) return null;
+        if (entry.isExpired()) {
+            store.remove(key);
+            return null;
+        }
+        return entry.getValue();
+    }
+
+    public boolean del(K key) {
+        return store.remove(key) != null;
+    }
+
+    public boolean exists(K key) {
+        return get(key) != null;
+    }
+
+    public long ttl(K key) {
+        CacheEntry<V> entry = store.get(key);
+
+        if (entry == null) return -1;
+        if (entry.isExpired()) {
+            store.remove(key);
+            return -1;
+        }
+
+        return entry.getRemainingTTL();
+    }
+
+    public void clear() {
+        store.clear();
+    }
 }
